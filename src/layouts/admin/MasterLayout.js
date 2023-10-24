161f0bd2ab2994ea4e4734b1dff6fae9a2cebaef
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-24 13:36:13 +0500

    create layouts , and paste code accordingly

---
+++ src/layouts/admin/MasterLayout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const MasterLayout = () => {
+    <div className="sb-nav-fixed">
+        <Navbar/>
+    </div>
+}
+
+export default MasterLayout;
