24b143833ceb3af5fa312dfb9cc47cdb2c023067
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-24 17:43:00 +0500

    create masterlayout for merge all layouts

--- src/layouts/admin/MasterLayout.js
+++ src/layouts/admin/MasterLayout.js
@@ -1,10 +1,26 @@
+import "../../assets/admin/css/styles.css";
+import "../../assets/admin/js/scripts";
+
 import React from "react";
 import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 
 const MasterLayout = () => {
+  return (
     <div className="sb-nav-fixed">
-        <Navbar/>
+      <Navbar />
+      <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+          <Sidebar />
+        </div>
+        <div id="layoutSidenav_content">
+          <main></main>
+          <Footer />
+        </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default MasterLayout;
