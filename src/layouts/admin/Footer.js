4987c6119da84dbc2cbe1873039875077d334b4a
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-27 18:32:55 +0500

    change class to className

--- src/layouts/admin/Footer.js
+++ src/layouts/admin/Footer.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer class="py-4 bg-light mt-auto">
-      <div class="container-fluid px-4">
-        <div class="d-flex align-items-center justify-content-between small">
-          <div class="text-muted">Copyright &copy; Your Website 2023</div>
+    <footer className="py-4 bg-light mt-auto">
+      <div className="container-fluid px-4">
+        <div className="d-flex align-items-center justify-content-between small">
+          <div className="text-muted">Copyright &copy; Your Website 2023</div>
           <div>
             <Link to="#">Privacy Policy</Link>
             <Link to="#">Terms Conditions</Link>
