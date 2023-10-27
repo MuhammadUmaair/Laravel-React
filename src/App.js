46d80127b43ea67b37c909b14847215311d4fa14
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-27 18:33:44 +0500

    set routes according to pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 import MasterLayout from "./layouts/admin/MasterLayout";
 import Dashboard from "./components/admin/Dashboard";
 import Profile from "./components/admin/Profile";
+import routes from "./routes/routes";
+import Home from "./components/admin/Home";
 
 function App() {
+
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<MasterLayout/>} />
-          <Route path="/admin/dashboard" element={<Dashboard/>} />
-          <Route path="/admin/profile" element={<Profile/>} />
+          <Route path={routes.home} element={<Home/>} />
+          <Route  path={routes.admin} element={<MasterLayout />}>
+            <Route path={routes.dashboard} element={<Dashboard />} /> {/* Relative path */}
+            <Route path={routes.profile} element={<Profile />} />
+          </Route>
         </Routes>
       </Router>
     </div>
