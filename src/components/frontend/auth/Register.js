d4c546280081f6006142608b683e581dc2b5ba2e
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-30 16:34:35 +0500

    create register and login page with complete form

--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../../../layouts/frontend/Navbar";
 
-function Register(){
-    return(
+function Register() {
+  return (
     <div>
-        Register
+      <Navbar />
+      <div className="container py-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header">
+                <h4>Register</h4>
+              </div>
+              <div className="card-body">
+                <form>
+                  <div className="form-group mb-3">
+                    <label>Full Name</label>
+                    <input
+                      type=""
+                      name="name"
+                      className="form-control"
+                      value=""
+                    />
+                  </div>
+                  <div className="form-group mb-3">
+                    <label>Email ID</label>
+                    <input
+                      type=""
+                      name="email"
+                      className="form-control"
+                      value=""
+                    />
+                  </div>
+                  <div className="form-group mb-3">
+                    <label>Password</label>
+                    <input
+                      type=""
+                      name="password"
+                      className="form-control"
+                      value=""
+                    />
+                  </div>
+                  <div className="form-group mb-3">
+                    <label>Confirm Password</label>
+                    <input
+                      type=""
+                      name="confirm_password"
+                      className="form-control"
+                      value=""
+                    />
+                  </div>
+
+                  <div className="form-group mb-3">
+                    <button type="submit" className="btn btn-primary" > Register</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default Register;
