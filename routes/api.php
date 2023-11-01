cf08aa0df3f9d0d2a310dd539972cb0b811b4353
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-11-01 20:22:08 +0500

    create register,login and logout apis

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register',  'register');
+    Route::post('login', 'login');
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
