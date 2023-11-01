19afe7f10f4a3912478973687f0066db317909d9
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-11-01 19:33:43 +0500

    add logout button, make condition as per requirements

--- src/layouts/frontend/Navbar.js
+++ src/layouts/frontend/Navbar.js
@@ -3,6 +3,34 @@ import { Link } from "react-router-dom";
 import routes from "../../routes/routes";
 
 function Navbar() {
+  var AuthButtons = "";
+  if (!localStorage.getItem("auth_token")) {
+    AuthButtons = (
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link" to={routes.login}>
+            Login
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link " to={routes.register}>
+            Register
+          </Link>
+        </li>
+      </ul>
+    );
+  } else {
+    AuthButtons = (
+      <li className="nav-item">
+        <button
+          type="button"
+          className="nav-link btn btn-danger btn-sm text-white"
+        >
+          Logout
+        </button>
+      </li>
+    );
+  }
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow stick-top">
       <div className="container">
@@ -23,7 +51,11 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to={routes.home}>
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to={routes.home}
+              >
                 Home
               </Link>
             </li>
@@ -32,16 +64,7 @@ function Navbar() {
                 Collections
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to={routes.login}>
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link " to={routes.register}>
-                Register
-              </Link>
-            </li>
+            {AuthButtons}
           </ul>
         </div>
       </div>
