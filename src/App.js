45d0281b6bd9bc2079df242e38698deaf26501f1
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-11-01 20:20:37 +0500

    add condition: if token then not access login/register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import MasterLayout from "./layouts/admin/MasterLayout";
 import Dashboard from "./components/admin/Dashboard";
 import Profile from "./components/admin/Profile";
@@ -13,15 +18,22 @@ axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("auth_token");
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  return config;
+});
 
 function App() {
+  const authToken = localStorage.getItem("auth_token");
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path={routes.home} element={<Home />} />
-          <Route path={routes.login} element={<Login />} />
-          <Route path={routes.register} element={<Register />} />
+          {/* check either is login or not by authToken */}
+          <Route path={routes.login} element={authToken ? <Navigate to="/" /> : <Login />}/>
+          <Route path={routes.register} element={authToken ? <Navigate to="/" /> : <Register />}/>
           <Route path={routes.admin} element={<MasterLayout />}>
             <Route path={routes.dashboard} element={<Dashboard />} />{" "}
             {/* Relative path */}
