d9155ca11f175b26b9a0fed0cec2a7f2302b9f34
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-11-01 19:20:49 +0500

    write signup conde

--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../../layouts/frontend/Navbar";
+import axios from "axios";
+import swal from "sweetalert";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const navigate = useNavigate();
+  const [registerInput, setRegister] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error_list: [],
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    setRegister({ ...registerInput, [e.target.name]: e.target.value });
+  };
+
+  const registerSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      name: registerInput.name,
+      email: registerInput.email,
+      password: registerInput.password,
+    };
+
+    console.log(data);
+    // SPA Authentication (To Generate Auth Token)
+    axios.get("/sanctum/csrf-cookie").then((response) => {
+      axios.post(`/api/register`, data).then((res) => {
+        if (res.data.status === 200) {
+          localStorage.setItem("auth_token", res.data.token);
+          localStorage.setItem("auth_name", res.data.username);
+          swal("Success", res.data.message, "success");
+          navigate("/");
+        } else {
+          setRegister({
+            ...registerInput,
+            error_list: res.data.validation_errors,
+          });
+        }
+      });
+    });
+  };
   return (
     <div>
       <Navbar />
@@ -13,46 +56,57 @@ function Register() {
                 <h4>Register</h4>
               </div>
               <div className="card-body">
-                <form>
+                <form onSubmit={registerSubmit}>
                   <div className="form-group mb-3">
                     <label>Full Name</label>
                     <input
                       type=""
                       name="name"
+                      onChange={handleInput}
+                      value={registerInput.name}
                       className="form-control"
-                      value=""
                     />
+                    <span>{registerInput.error_list.name}</span>
                   </div>
                   <div className="form-group mb-3">
                     <label>Email ID</label>
                     <input
                       type=""
                       name="email"
+                      onChange={handleInput}
+                      value={registerInput.email}
                       className="form-control"
-                      value=""
                     />
+
+                    <span>{registerInput.error_list.email}</span>
                   </div>
                   <div className="form-group mb-3">
                     <label>Password</label>
                     <input
                       type=""
                       name="password"
+                      onChange={handleInput}
+                      value={registerInput.password}
                       className="form-control"
-                      value=""
                     />
+
+                    <span>{registerInput.error_list.password}</span>
                   </div>
                   <div className="form-group mb-3">
                     <label>Confirm Password</label>
                     <input
                       type=""
                       name="confirm_password"
+                      onChange={handleInput}
+                      value={registerInput.confirm_password}
                       className="form-control"
-                      value=""
                     />
                   </div>
 
                   <div className="form-group mb-3">
-                    <button type="submit" className="btn btn-primary" > Register</button>
+                    <button type="submit" className="btn btn-primary">
+                      Register
+                    </button>
                   </div>
                 </form>
               </div>
