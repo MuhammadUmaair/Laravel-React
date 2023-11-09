e83cc52c95dbbe03bb34e8a939a4a90453cb98b6
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-11-09 17:03:00 +0500

    create error pages

---
+++ src/components/errors/Page403.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Page403() {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card card-body">
+            <h1>page 403 | Forbidden</h1>
+            <h1>Access Denied.! As You Are Not An Admin</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page403;
---
+++ src/components/errors/Page404.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Page404() {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card card-body">
+            <h1>Page 403 | Page Not Found</h1>
+            <br/>
+            <h1>Url/Page You Are Searching Not Found.! </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page404;
