1cf81bfcad67eb96506d74459b10de8e0c555bc6
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-11-09 19:38:35 +0500

    create admin route in another const, so we run separately

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Route,
   Routes,
+  Route,
   Navigate,
 } from "react-router-dom";
-import MasterLayout from "./layouts/admin/MasterLayout";
-import Dashboard from "./components/admin/Dashboard";
-import Profile from "./components/admin/Profile";
 import routes from "./routes/routes";
+import Home from "./components/frontend/Home";
 import Login from "./components/frontend/auth/Login";
 import Register from "./components/frontend/auth/Register";
-import Home from "./components/frontend/Home";
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:8000/";
-axios.defaults.headers.post["Content-Type"] = "application/json";
-axios.defaults.headers.post["Accept"] = "application/json";
-axios.defaults.withCredentials = true;
-axios.interceptors.request.use(function (config) {
-  const token = localStorage.getItem("auth_token");
-  config.headers.Authorization = token ? `Bearer ${token}` : "";
-  return config;
-});
+import E403 from "./components/errors/Page403";
+import E404 from "./components/errors/Page404";
+import AdminPrivateRoute from "./components/admin/AdminPrivateRoute";
 
 function App() {
   const authToken = localStorage.getItem("auth_token");
+  const AdminToken = localStorage.getItem("admin_token");
+  const renderRoutes = () => (
+    <Routes>
+      <Route path={routes.home} element={<Home />} />
+      <Route path={routes.e403} element={<E403 />} />
+      <Route path={routes.e404} element={<E404 />} />
+      <Route
+        path={routes.login}
+        element={authToken ? <Navigate to="/" /> : <Login />}
+      />
+      <Route
+        path={routes.register}
+        element={authToken ? <Navigate to="/" /> : <Register />}
+      />
+    </Routes>
+  );
+  const adminRoutes = () => (
+    // <Routes>
+    //   <Route path={routes.admin} element={<MasterLayout />}>
+    //     <Route path={routes.dashboard} element={<Dashboard />} />
+    //     <Route path={routes.profile} element={<Profile />} />
+    //   </Route>
+    // </Routes>
+    <AdminPrivateRoute authToken={authToken}/>
+  );
+
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path={routes.home} element={<Home />} />
-          {/* check either is login or not by authToken */}
-          <Route path={routes.login} element={authToken ? <Navigate to="/" /> : <Login />}/>
-          <Route path={routes.register} element={authToken ? <Navigate to="/" /> : <Register />}/>
-          <Route path={routes.admin} element={<MasterLayout />}>
-            <Route path={routes.dashboard} element={<Dashboard />} />{" "}
-            {/* Relative path */}
-            <Route path={routes.profile} element={<Profile />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Router>{renderRoutes()}</Router>
+      <Router>{AdminToken && adminRoutes()}</Router>
     </div>
   );
 }
